f4627c08c4a66409b853e7b7b72c25ebef027afd
QuanHao <1660206164@qq.com>
2023-03-16 17:14:23 +0800

    。。

--- src/view/UserTest.java
+++ src/view/UserTest.java
@@ -3,7 +3,7 @@ package view;
 import pojo.User;
 
 public class UserTest {
-
+    // 这是个测试
     public static void main(String[] args) {
         User u = new User(101,"zhangsan");
         System.out.println(u);

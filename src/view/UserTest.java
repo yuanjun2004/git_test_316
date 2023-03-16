b2c314b05961eaf379cc7c88461736a5f6d63712
yuanjun <370040986@qq.com>
2023-03-16 17:16:59 +0800

    add system

--- src/view/UserTest.java
+++ src/view/UserTest.java
@@ -7,5 +7,7 @@ public class UserTest {
     public static void main(String[] args) {
         User u = new User(101,"zhangsan");
         System.out.println(u);
+
+        System.out.println("git test");
     }
 }

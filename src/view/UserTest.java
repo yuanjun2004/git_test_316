e2b9d797b4bf8054aae16504bc74d606be134f4c
yuanjun <370040986@qq.com>
2023-03-16 17:06:15 +0800

    Initial commit

---
+++ src/view/UserTest.java
@@ -0,0 +1,11 @@
+package view;
+
+import pojo.User;
+
+public class UserTest {
+
+    public static void main(String[] args) {
+        User u = new User(101,"zhangsan");
+        System.out.println(u);
+    }
+}
